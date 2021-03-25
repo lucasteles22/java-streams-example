10664b67728222d42405a033a6c731935bbfbdc7
Lucas Teles <lucasteles22@gmail.com>
2021-03-24 23:16:29 -0300

    Remove redundance between String.format() and System.out.println()

--- src/com/streams/example/io/Log.java
+++ src/com/streams/example/io/Log.java
@@ -2,9 +2,9 @@ package com.streams.example.io;
 
 public final class Log {
     public static void Output(String label, double maxDistance, double time, long memoryUsage) {
-        System.out.println(String.format("Max distance: %,.4f", maxDistance));
+        System.out.printf("Max distance: %,.4f%n", maxDistance);
         System.out.println("------ METRICS ------");
-        System.out.println(String.format("%s time (milliseconds): %,.4f", label, time));
-        System.out.println(String.format("Memory usage (bytes): %d", memoryUsage));
+        System.out.printf("%s time (milliseconds): %,.4f%n", label, time);
+        System.out.printf("Memory usage (bytes): %d%n", memoryUsage);
     }
 }
