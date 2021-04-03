98c6cafd41363c7a8eb10b62e8f622c6b49297f6
Lucas Teles <lucasteles22@gmail.com>
2021-04-03 14:59:57 -0300

    Refactor strategy implementation class

--- src/com/streams/example/strategies/StrategyImpl.java
+++ src/com/streams/example/strategies/StrategyImpl.java
@@ -18,8 +18,19 @@ public abstract class StrategyImpl implements Strategy {
         this.label = label;
     }
 
-    protected List<Integer> getIntegers() {
+    /*
+    Creates a List with values following this pattern:
+    1. Integer's values.
+    2. From 0 until SIZE_INTEGERS_LIST
+    For example, if SIZE_INTEGERS_LIST is equal to 10, the List has the values:
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+     */
+    protected final List<Integer> getIntegers() {
         return IntStream.rangeClosed(0, SIZE_INTEGERS_LIST).boxed().collect(Collectors.toList());
+    }
+
+    protected final double getDuration() {
+        return Duration.between(this.start, this.end).toMillis();
     }
 
     protected String getLabel() {
@@ -32,10 +43,6 @@ public abstract class StrategyImpl implements Strategy {
 
     protected void setEnd() {
         this.end = Instant.now();
-    }
-
-    protected final double getDuration() {
-        return Duration.between(this.start, this.end).toMillis();
     }
 
     protected long getMemoryUsage() {
